dd73a30150945271b910784336fd0fd29e7e4a3a
astex <0astex@gmail.com>
2014-09-11 01:22:10 -0400

    jquery setup; shim additions; pure

--- static/main.js
+++ static/main.js
@@ -1,8 +1,15 @@
+// Admin main
+//
+// This is the main execution loop of the admin panel. It provides basic
+// configuration, routing, and default imports.
+
 require.config({
   paths: {
-    jquery: 'lib/jquery-1.10.2.min',
-    underscore: 'lib/underscore',
-    backbone: 'lib/backbone'
+    'jquery': 'lib/jquery-1.10.2.min',
+    'underscore': 'lib/underscore',
+    'backbone': 'lib/backbone',
+    'require-css': 'lib/require-css.min',
+    'pure': '//yui.yahooapis.com/pure/0.5.0/pure-min'
   },
   shim: {
     jquery: {
@@ -15,10 +22,15 @@ require.config({
       deps: ['jquery', 'underscore'],
       exports: 'Backbone'
     }
+  },
+  map: {
+    '*': {
+      'css': 'require-css'
+    }
   }
 });
 
-require(['backbone'], function(Backbone) {
+require(['jquery', 'backbone', 'css!pure'], function($, Backbone) {
   var
     Router = Backbone.Router.extend({
       routes: {
@@ -36,6 +48,16 @@ require(['backbone'], function(Backbone) {
       }
     }),
     router = new Router();
+
+  // Configure the global app header.
+  $.ajaxSetup({
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader(
+        'x-peanuts-application',
+        '6752e4b0-8122-4c9a-ad1d-19d703fdfed0'
+      );
+    }
+  });
 
   Backbone.history.start();
 });
