8afced59cf37f30ae12e7da27ab3bd20c5d1c397
astex <0astex@gmail.com>
2014-09-11 10:17:03 -0400

    first use view

--- static/views/first-use.js
+++ static/views/first-use.js
@@ -1,3 +1,27 @@
-define(['backbone'], function(Backbone) {
-  return function() {};
-});
+define(
+  [
+    'underscore',
+    'backbone',
+    'text!templates/first-use.utpl',
+    'css!styles/first-use'
+  ], function(
+    _,
+    Backbone,
+    FirstUseTemplate
+  ) {
+    return function() {
+      var View = Backbone.View.extend({
+        el: 'body',
+        template: _.template(FirstUseTemplate),
+        title: 'Register First User',
+        render: function() {
+          this.$el
+            .addClass('first-use')
+            .html(this.template());
+        }
+      });
+
+      return new View();
+    };
+  }
+);
